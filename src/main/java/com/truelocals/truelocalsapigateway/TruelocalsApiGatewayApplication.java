aa737811ad1e1d9b9f0715ac91f19f5df7b48cee
David Roman <dromancamps@gmail.com>
2025-02-05 22:01:41 +0100

    Initial commit

---
+++ src/main/java/com/truelocals/truelocalsapigateway/TruelocalsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.truelocals.truelocalsapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruelocalsApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruelocalsApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/truelocals/truelocalsapigateway/TruelocalsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truelocals.truelocalsapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruelocalsApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
